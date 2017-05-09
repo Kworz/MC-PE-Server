7d7c0dc96b4cd21a0a989c4f1b6d5ce137660261
xblur <azared601@gmail.com>
2017-05-09 21:06:41 +0200

    ID_UNCONNECTED_PONG works

--- Main.cpp
+++ Main.cpp
@@ -1,39 +1,41 @@
 #include <stdio.h>
-#include "RakPeerInterface.h"
-#include "MessageIdentifiers.h"
-#include "PacketPriority.h"
+#include <iostream>
 #include <string>
+#include <vector>
+
+#include "RakNet/RakPeerInterface.h"
+#include "RakNet/MessageIdentifiers.h"
+#include "RakNet/PacketPriority.h"
 
 #define SERVER_PORT 19132
 #define MAX_CLIENTS 20
 
+using namespace std;
 
 int main(void)
 {
+	vector<int> motd;
+
+	//Hello wurld
 	printf("Starting MC++ PE Server \n");
 	
+	//Raknet stuff
 	RakNet::RakPeerInterface *peer = RakNet::RakPeerInterface::GetInstance();
 	RakNet::Packet *packet;
-
 	RakNet::SocketDescriptor sd(SERVER_PORT, 0);
 	peer->Startup(MAX_CLIENTS, &sd, 1);
-
 	peer->SetMaximumIncomingConnections(MAX_CLIENTS);
+	
+	string message = "MCPE;Blyat;107;1.0.7;0;20";
 
-	const char* message = "MCPE;TEST server for PE;107;1.0.7;0;20";
+	
+	message.insert(message.begin(), message.size());
+	message.insert(message.begin(), 0x00);
 
-	peer->SetOfflinePingResponse(message, 38);
+	peer->SetOfflinePingResponse(message.c_str(), message.size());
 
 	while(1)
 	{
-		for(packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
-		{
-			switch(packet->data[0])
-			{
-				default:
-					//printf("else\n");
-					break;
-			}
-		}
+
 	}
 }
