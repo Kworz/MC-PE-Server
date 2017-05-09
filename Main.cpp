b1e07ec6cec4343d3dd614b0de55d9daff21fd19
xblur <azared601@gmail.com>
2017-05-09 21:59:40 +0200

    Huge CPU usage fixed, Protocol version ajusted

--- Main.cpp
+++ Main.cpp
@@ -3,21 +3,22 @@
 #include <string>
 #include <vector>
 
-#include "RakNet/RakPeerInterface.h"
-#include "RakNet/MessageIdentifiers.h"
-#include "RakNet/PacketPriority.h"
+#include "RakPeerInterface.h"
+#include "MessageIdentifiers.h"
+#include "PacketPriority.h"
+#include "RakNetVersion.h"
 
 #define SERVER_PORT 19132
 #define MAX_CLIENTS 20
+#define TPS 20
 
 using namespace std;
 
 int main(void)
 {
-	vector<int> motd;
-
 	//Hello wurld
 	printf("Starting MC++ PE Server \n");
+	printf("Protocal version : %d.\n", RAKNET_PROTOCOL_VERSION);
 	
 	//Raknet stuff
 	RakNet::RakPeerInterface *peer = RakNet::RakPeerInterface::GetInstance();
@@ -26,16 +27,18 @@ int main(void)
 	peer->Startup(MAX_CLIENTS, &sd, 1);
 	peer->SetMaximumIncomingConnections(MAX_CLIENTS);
 	
+	//Motd
 	string message = "MCPE;Blyat;107;1.0.7;0;20";
 
-	
+	//Adding size of packet (fk u raknet)
 	message.insert(message.begin(), message.size());
 	message.insert(message.begin(), 0x00);
 
 	peer->SetOfflinePingResponse(message.c_str(), message.size());
 
+	//infinite loop + tps
 	while(1)
 	{
-
+		Sleep(1000 / TPS);
 	}
 }
